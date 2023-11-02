073ee53a0bd92bb75e5db3c62829973822d6b64f
michico929 <132318861+michico929@users.noreply.github.com>
2023-11-02 09:37:22 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -39,13 +39,13 @@ const BLOG = {
 
   // START ************网站字体*****************
 
-  FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-sans', // ['font-serif','font-sans'] 两种可选，分别是衬线和无衬线: 参考 https://www.jianshu.com/p/55e410bd2115
+  FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-serif', // ['font-serif','font-sans'] 两种可选，分别是衬线和无衬线: 参考 https://www.jianshu.com/p/55e410bd2115
   // 字体CSS 例如 https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css
   FONT_URL: [
-    // 'https://npm.elemecdn.com/Microsoft-YaHei-webfont@1.6.0/style.css',
-    'https://fonts.googleapis.com/css?family=Arial&display=swap',
-    'https://fonts.googleapis.com/css2?family=Microsoft-YaHei:wght@300&display=swap',
-    'https://fonts.googleapis.com/css2?family=Microsoft-YaHei:wght@300&display=swap'
+    // 'https://npm.elemecdn.com/SimSun-webfont@1.6.0/style.css',
+    'https://fonts.googleapis.com/css?family=serif&display=swap',
+    'https://fonts.googleapis.com/css2?family=SimSun:wght@300&display=swap',
+    'https://fonts.googleapis.com/css2?family=SimSun:wght@300&display=swap'
   ],
   // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
